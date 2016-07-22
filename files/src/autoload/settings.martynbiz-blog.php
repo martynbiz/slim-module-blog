c37da1a19655297b7e05061a17c721f1c625b425
Martyn Bissett <m@rtyn.biz>
2016-07-22 22:37:49 +0900

    Move files

--- files/src/autoload/settings.martynbiz-blog.php
+++ files/src/autoload/settings.martynbiz-blog.php
@@ -1,13 +1,6 @@
 <?php
 return [
     'settings' => [
-
-        'renderer' => [
-            'folders' => [
-                APPLICATION_PATH . '/src/martynbiz-blog/templates',
-            ],
-        ],
-
         'photos_dir' => [
             'original' => APPLICATION_PATH . '/data/photos',
             'cache' => APPLICATION_PATH . '/data/photos/cache',
